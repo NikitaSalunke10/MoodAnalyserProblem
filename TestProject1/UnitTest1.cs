ef424005bc1f86814b43b0e1ca49d7e18cf7ba66
NikitaSalunke10 <salunkenikita10@gmail.com>
2022-08-08 20:05:07 +0530

    Analysed the mood happy or sad added

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -6,13 +6,13 @@ namespace TestProject1
     public class UnitTest1
     {
         [TestMethod]
-        public void GivenSadMoodShouldReturnSad()
+        public void GivenSadMoodShouldReturnSad() // this method is used to check whether the expected mood and actual mood match or not
         {
             string expected = "SAD";
             string message = "I am in sad mood.";
-            MoodAnalyser moodAnalyser = new MoodAnalyser(message);
-            string mood = moodAnalyser.AnalyseMood();
-            Assert.AreEqual(expected, mood);
+            MoodAnalyser moodAnalyser = new MoodAnalyser(message); //created object and passed the message to MoodAnalyser Constructor
+            string mood = moodAnalyser.AnalyseMood(); //the value return from AnalyseMood method is stored in mood
+            Assert.AreEqual(expected, mood); // checking whether both values are equal or not
         }
     }
 }
