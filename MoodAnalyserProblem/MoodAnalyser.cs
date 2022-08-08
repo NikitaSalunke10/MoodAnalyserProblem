b6ed082d01991199a40f91bc306445d1bd743e95
NikitaSalunke10 <salunkenikita10@gmail.com>
2022-08-08 20:01:00 +0530

    Analysed the mood happy or sad added

--- MoodAnalyserProblem/MoodAnalyser.cs
+++ MoodAnalyserProblem/MoodAnalyser.cs
@@ -9,17 +9,17 @@ namespace MoodAnalyserProblem
     public class MoodAnalyser
     {
         private string message;
-        public MoodAnalyser(string message)
+        public MoodAnalyser(string message) // created a constructor to assign the message received from UnitTest1 class
         {
             this.message = message;
         }
-        public string AnalyseMood()
+        public string AnalyseMood() // this method is used to analyse the mood
         {
-            if (this.message.Contains("sad"))
+            if (this.message.Contains("sad")) // if the message contains the word sad then this condition becomes true and returns SAD
             {
                 return "SAD";
             }
-            else
+            else // if condition becomes false then it will return HAPPY
             {
                 return "HAPPY";
             }
